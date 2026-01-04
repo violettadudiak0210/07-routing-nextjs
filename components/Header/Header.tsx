4ce2e46b213cfbc6ed65300e925796c9c2f7a4c7
violettadudiak0210 <violettadudiak0210@gmail.com>
2026-01-04 12:51:16 +0000

    fix

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,24 +1,38 @@
 import TagsMenu from '../TagsMenu/TagsMenu';
-import css from './Header.module.css'
-import Link from 'next/link'
+import css from './Header.module.css';
+import Link from 'next/link';
 
 const Header = () => {
-  return <header className={css.header}>
-  <Link href="/" aria-label="Home" className={css.headerLink}>
-    NoteHub
-  </Link>
-  <nav aria-label="Main Navigation">
-    <ul className={css.navigation}>
-      <li className={css.navigationItem}>
-        <Link href="/" className={css.navigationLink}>Home</Link>
-      </li>
-      <li>
-        <TagsMenu />
-      </li>
-    </ul>
-  </nav>
-</header>
+  return (
+    <header className={css.header}>
+      <Link href="/" aria-label="Home" className={css.headerLink}>
+        NoteHub
+      </Link>
 
-}
+      <nav aria-label="Main Navigation">
+        <ul className={css.navigation}>
+          <li className={css.navigationItem}>
+            <Link href="/" className={css.navigationLink}>
+              Home
+            </Link>
+          </li>
+
+          <li className={css.navigationItem}>
+            <Link
+              href="/notes/filter/all"
+              className={css.navigationLink}
+            >
+              Notes
+            </Link>
+          </li>
+
+          <li className={css.navigationItem}>
+            <TagsMenu />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
